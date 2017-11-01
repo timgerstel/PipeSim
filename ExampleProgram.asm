a50005dc6b67c61f4756a60ed492e396180b6bc7
Timothy Gerstel <tgerstel@umass.edu>
2017-10-31 23:31:03 -0400

    Added consecutive comma to test functionality of progScanner()

--- ExampleProgram.asm
+++ ExampleProgram.asm
@@ -1,7 +1,7 @@
 #Example Simulation File
 #C code: d[2] = d[1] + a*b;
 lw $t0, 4($s4)      # $t0 = d[1] 
-lw $t1, 0($s1)      # $t1 = a
+lw $t1,, 0($s1)      # $t1 = a
 lw $t2,  0($s2)      # $t2 = b
 mul $t1, $t1, $t2   # $t1 = = $t1 * $t2 = a*b
 add $t0,  $t0, $t1   # $t0 = d[1] + a*b
