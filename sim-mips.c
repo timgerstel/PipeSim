4c600adb50858937442d25b7d61dee4366a8fef7
Timothy Gerstel <tgerstel@umass.edu>
2017-11-09 20:36:40 -0500

    Remvoed regNames array

--- sim-mips.c
+++ sim-mips.c
@@ -14,18 +14,6 @@
 #define REG_NUM 32
 #define MEM 512
 
-static char* regNames[] = {
-	"zero",
-	"at",
-	"v0", "v1",
-	"a0", "a1", "a2", "a3",
-	"t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7",
-	"s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7",
-	"t8", "t9",
-	"k0", "k1",
-	"gp", "sp", "fp", "ra"
-};
-
 static char* opNames[]= {
 	"add", "addi",
 	"sub",
